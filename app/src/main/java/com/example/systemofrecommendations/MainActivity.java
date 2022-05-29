2d95ae29600f570af51b30771fd460ac72b1066e
larrysfinolb <larrysfinolb@gmail.com>
2022-05-29 00:06:38 -0400

    create array shoppingHistory

--- app/src/main/java/com/example/systemofrecommendations/MainActivity.java
+++ app/src/main/java/com/example/systemofrecommendations/MainActivity.java
@@ -6,6 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String shoppingHistory[][] = {
+            {"Samuel", "Guantes", "Moby Dick(novela)", "Audifonos", "Lentes para sol", "Café"},
+            {"Juan", "Fanela deportiva", "Café", "Cafetera", "Café", "Café"},
+            {"Janina", "Lentes para sol", "Zapatos deportivos", "Franela deportiva", "Zapatos deportivos", "Calcetines"},
+            {"Henrique", "2001: A Space Odyssey(dvd)", "Audífonos", "Franela deportiva", "Guantes", "Sandalias"},
+            {"Victor", "Franela deportiva", "Sandalias", "Lentes para sol", "Moby Dick(novela)", "Protector solar"},
+            {"Tobias", "Moby Dick(novela)", "Café", "2001: A Space Odyssey(dvd)", "Audífonos", "Café"}
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
